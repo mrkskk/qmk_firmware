e9bb57293a4f2b2ce98c14d1fc578df4a40c6fcb
mrkskk <61738474+mrkskk@users.noreply.github.com>
2021-02-15 17:12:10 +0100

    Updated Combos

--- users/mrkskk/combos.c
+++ users/mrkskk/combos.c
@@ -4,31 +4,40 @@
 enum combo_events {
   COMBO_NR_1,
   COMBO_NR_2,
+  COMBO_NR_3,
+
 };
 
-const uint16_t PROGMEM OUTER_RIGHT_THUMB_COMBO[] = { KC_LCTRL, KC_TRNS, COMBO_END};
-//const uint16_t PROGMEM INNER_RIGHT_THUMB_COMBO[] = { NVEN, S_S, COMBO_END};
+const uint16_t PROGMEM OUTER_RIGHT_THUMB_COMBO[] = { ____, CBSPC, COMBO_END};
+const uint16_t PROGMEM INNER_RIGHT_THUMB_COMBO[] = { NVENT, S_S, COMBO_END};
+const uint16_t PROGMEM INNER_LEFT_THUMB_COMBO[] = { FIND, NVSPC, COMBO_END};
+
+
+
 
 combo_t key_combos[COMBO_COUNT] = {
   [COMBO_NR_1] = COMBO_ACTION(OUTER_RIGHT_THUMB_COMBO),
- // [COMBO_NR_2] = COMBO_ACTION(INNER_RIGHT_THUMB_COMBO),
-
+  [COMBO_NR_2] = COMBO_ACTION(INNER_RIGHT_THUMB_COMBO),
+  [COMBO_NR_3] = COMBO_ACTION(INNER_LEFT_THUMB_COMBO),
 };
 
 void process_combo_event(uint16_t combo_index, bool pressed) {
   switch(combo_index) {
     case COMBO_NR_1:
       if (pressed) {
-        layer_on(SHORTCUTS);
-      } else {
-        layer_off(SHORTCUTS);
+        tap_code16(BSPC_W);
       }
       break;
-/*      case COMBO_NR_2:
+      case COMBO_NR_2:
       if (pressed) {
-        SEND_STRING(SS_DELAY(100)"test\n");
+        tap_code16(DEL_W);
       }
       break;
-*/ }
+      case COMBO_NR_3:
+      if (pressed) {
+        tap_code16(S(MINUS));
+      }
+      break;
+ }
 }
 
