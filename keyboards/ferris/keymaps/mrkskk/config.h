22b4ed3e37b1997bc0b97fe45daa8ec0254fe576
mrkskk <61738474+mrkskk@users.noreply.github.com>
2021-06-30 11:31:31 +0200

    remove all ferris files

--- keyboards/ferris/keymaps/mrkskk/config.h
+++
@@ -1,4 +0,0 @@
-
-#ifdef RGB_MATRIX_ENABLE
-#    define RGB_DISABLE_WHEN_USB_SUSPENDED true
-#endif
--- keyboards/ferris/keymaps/mrkskk/rules.mk
+++
@@ -1,5 +0,0 @@
-
-OLED_DRIVER_ENABLE = no   # Enables the use of OLED displays
-ENCODER_ENABLE = no       # Enables the use of one or more encoders
-
-
