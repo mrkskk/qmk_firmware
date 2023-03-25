c1f0f99a0432193d32b93f9627eb3008c4635d27
mrkskk <61738474+mrkskk@users.noreply.github.com>
2023-03-25 17:08:02 +0100

    My_keymap upload

---
+++ keyboards/bastardkb/charybdis/3x5/keymaps/mrkskk/config.h
@@ -0,0 +1,42 @@
+// clang-format on
+#pragma once
+
+#define TAPPING_TERM 160
+
+#ifdef COMBO_ENABLE
+// #    define COMBO_MUST_PRESS_IN_ORDER_PER_COMBO
+#    define COMBO_MUST_TAP_PER_COMBO
+// #    define COMBO_TERM_PER_COMBO
+#    define COMBO_TERM 15
+#    define COMBO_ONLY_FROM_LAYER 0
+#endif
+
+#ifdef POINTING_DEVICE_ENABLE
+// #    define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+#    define CHARYBDIS_AUTO_SNIPING_ON_LAYER
+
+// #    define CHARYBDIS_DRAGSCROLL_REVERSE_X
+#    define CHARYBDIS_DRAGSCROLL_REVERSE_Y
+#endif // POINTING_DEVICE_ENABLE
+
+#ifdef AUTO_SHIFT_ENABLE
+// #    define RETRO_SHIFT 200
+#    define AUTO_SHIFT_TIMEOUT 160
+#    define NO_AUTO_SHIFT_SPECIAL
+#    define NO_AUTO_SHIFT_ALPHA
+// #    define NO_AUTO_SHIFT_NUMERIC
+#    define AUTO_SHIFT_NO_AUTO_REPEAT
+#endif
+
+// saving bytes
+#define NO_MUSIC_MODE     // not using speakers
+#define LAYER_STATE_16BIT // maximum of 7 layers
+
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+#define NO_ACTION_ONESHOT // disable One shot layers and mods
+
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+
+// #define DEBOUNCE 10 //if problems with chatter
---
+++ keyboards/bastardkb/charybdis/3x5/keymaps/mrkskk/features/os_keys.h
@@ -0,0 +1,9 @@
+
+// clang-format on
+
+#pragma once
+#include QMK_KEYBOARD_H
+
+bool is_windows(void);
+bool is_mac(void);
+// static void toggle_unicode_mode(void);
